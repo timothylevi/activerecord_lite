2c14a49214c432f8f0414e52987008ffa9d0c50b
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 20:22:52 -0500

    Writes AttrAccessorObject::my_attr_accessor

--- lib/active_record_lite/00_attr_accessor_object.rb
+++ lib/active_record_lite/00_attr_accessor_object.rb
@@ -1,5 +1,14 @@
 class AttrAccessorObject
+
   def self.my_attr_accessor(*names)
-    # ...
+    names.each do |name|
+      define_method("#{name}".to_sym) do
+        instance_variable_get("@#{name}".to_sym)
+      end
+
+      define_method("#{name}=".to_sym) do |value=nil|
+        instance_variable_set("@#{name}".to_sym, value)
+      end
+    end
   end
 end
