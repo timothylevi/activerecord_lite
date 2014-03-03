1970795703192d42bd5391da089ea6d996523e0a
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 21:49:54 -0500

    Removes notes from 'associatable2.rb'

--- lib/active_record_lite/05_associatable2.rb
+++ lib/active_record_lite/05_associatable2.rb
@@ -1,10 +1,7 @@
 require_relative '04_associatable'
 
-# Phase V
 module Associatable
-  # Remember to go back to 04_associatable to write ::assoc_options
 
   def has_one_through(name, through_name, source_name)
-    # ...
   end
 end
