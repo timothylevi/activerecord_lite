8f92f3f0c874bf6569360295a66316954d255f0a
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 20:31:38 -0500

    Writes MassObject#initialize to take in params and check if they are mass-assignable or not

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -10,4 +10,13 @@ class MassObject < AttrAccessorObject
     self.attributes.concat(new_attributes)
   end
 
+  def initialize(params = {})
+    params.each do |attr_name, value|
+      if self.class.attributes.include?("#{attr_name}".to_sym)
+        self.send("#{attr_name}=".to_sym, value)
+      else
+        raise "mass assignment to unregistered attribute '#{attr_name}'"
+      end
+    end
+  end
 end
