a1963c2ad0720fcdf5b85b1bf511533597f632d9
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 21:23:59 -0500

    Adds a Searchable module and Searchable::where to search in a DB given columns and values

--- lib/active_record_lite/03_searchable.rb
+++ lib/active_record_lite/03_searchable.rb
@@ -3,7 +3,16 @@ require_relative '02_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    col_names = params.keys.map { |key| "#{key} = ?"}.join(" AND ")
+    row_values = params.values
+
+    results = DBConnection.execute(<<-SQL, *row_values)
+    SELECT *
+    FROM #{table_name}
+    WHERE #{col_names}
+    SQL
+
+    parse_all(results)
   end
 end
 
