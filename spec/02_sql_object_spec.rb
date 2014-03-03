9ed41dcc57dd327c6facc6a30f8ffdb14a33566f
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 20:43:06 -0500

    Updates spec

--- spec/02_sql_object_spec.rb
+++ spec/02_sql_object_spec.rb
@@ -7,10 +7,15 @@ describe SQLObject do
 
   before(:all) do
     class Cat < SQLObject
+      my_attr_accessor :id, :name, :owner_id
+      my_attr_accessible :id, :name, :owner_id
     end
 
     class Human < SQLObject
       self.table_name = "humans"
+
+      my_attr_accessor :id, :fname, :lname, :house_id
+      my_attr_accessible :id, :fname, :lname, :house_id
     end
   end
 
@@ -86,9 +91,9 @@ describe SQLObject do
     it "#update changes attributes" do
       human = Human.find(2)
 
-      human.fname = "Matthew"
-      human.lname = "von Rubens"
-      human.update
+    human.fname = "Matthew"
+    human.lname = "von Rubens"
+    human.update
 
       # pull the human again
       human = Human.find(2)
