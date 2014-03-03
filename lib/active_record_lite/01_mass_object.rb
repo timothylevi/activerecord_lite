c4503bd37c43d94f2be96b904a3ccfb76d50af99
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 20:30:24 -0500

    Writes MassObject::attributes and MassObject::my_attr_accessible

--- lib/active_record_lite/01_mass_object.rb
+++ lib/active_record_lite/01_mass_object.rb
@@ -1,16 +1,13 @@
-# deprecated for Rails 4
-# require_relative '00_attr_accessor_object.rb'
-#
-# class MassObject < AttrAccessorObject
-#   def self.my_attr_accessible(*new_attributes)
-#     # ...
-#   end
-#
-#   def self.attributes
-#     # ...
-#   end
-#
-#   def initialize(params = {})
-#     # ...
-#   end
-# end
+require_relative '00_attr_accessor_object.rb'
+
+class MassObject < AttrAccessorObject
+  def self.attributes
+    raise Exception, "must not call #attributes on MassObject directly" if self == MassObject
+    @attributes ||= []
+  end
+
+  def self.my_attr_accessible(*new_attributes)
+    self.attributes.concat(new_attributes)
+  end
+
+end
