1d67e4a60a9572d583bc92eef8da03642aef3a78
Timothy Levi Campbell <tlc9406@gmail.com>
2014-03-02 21:29:55 -0500

    Adds BelongsToOptions#initialize

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -13,37 +13,45 @@ class AssocOptions
   end
 
   def table_name
-    # ...
+
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {
+      :class_name => name.to_s.camelcase,
+      :foreign_key => "#{name}_id".to_sym,
+      :primary_key => :id
+    }
+
+    defaults.keys.each do |key|
+      self.send("#{key}=", options[key] || defaults[key])
+    end
   end
 end
 
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
-    # ...
+
   end
 end
 
 module Associatable
 
   def belongs_to(name, options = {})
-    # ...
+
   end
 
   def has_many(name, options = {})
-    # ...
+
   end
 
   def assoc_options
-    # Wait to implement this in Phase V. Modify `belongs_to`, too.
+
   end
 end
 
 class SQLObject
-  # Mixin Associatable here...
+
 end
